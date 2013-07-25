4ec1f8f99a53167fe0c6b2eb0130e2efa26d0136
Gyeongsu Kim <nabugoon@gmail.com>
2013-07-25 04:00:13 +0000

    Updated web.js to read index.html file

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content = "";
+  fs.readFile('index.html', function(err, data) {
+    if (err) throw err;
+    content = data.toString();
+    response.send(content);
+  });
 });
 
 var port = process.env.PORT || 5000;
